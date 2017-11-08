71ee793923490ab60b20eb2adc32849a2f60a40d
L kabeer <ahmed@Lkabeer.com>
2017-11-08 21:15:44 +0200

    version 2.1

--- js/script.js
+++ js/script.js
@@ -3,31 +3,8 @@ var centerPointX = {
 	lng: 140.887
 }  
 
-var locations = [
-	{lat: -31.563910, lng: 147.154312},
-	{lat: -33.718234, lng: 150.363181},
-	{lat: -33.727111, lng: 150.371124},
-	{lat: -33.848588, lng: 151.209834},
-	{lat: -33.851702, lng: 151.216968},
-	{lat: -34.671264, lng: 150.863657},
-	{lat: -35.304724, lng: 148.662905},
-	{lat: -36.817685, lng: 175.699196},
-	{lat: -36.828611, lng: 175.790222},
-	{lat: -37.750000, lng: 145.116667},
-	{lat: -37.759859, lng: 145.128708},
-	{lat: -37.765015, lng: 145.133858},
-	{lat: -37.770104, lng: 145.143299},
-	{lat: -37.773700, lng: 145.145187},
-	{lat: -37.774785, lng: 145.137978},
-	{lat: -37.819616, lng: 144.968119},
-	{lat: -38.330766, lng: 144.695692},
-	{lat: -39.927193, lng: 175.053218},
-	{lat: -41.330162, lng: 174.865694},
-	{lat: -42.734358, lng: 147.439506},
-	{lat: -42.734358, lng: 147.501315},
-	{lat: -42.735258, lng: 147.438000},
-	{lat: -43.999792, lng: 170.463352}
-  ];
+var markers = [];
+var info = [];
 
 function initMap() {
 	var map = new google.maps.Map(document.getElementById('map'), {
@@ -35,27 +12,44 @@ function initMap() {
 		center: centerPointX
 	});
 	
+	// load markers & cluster them X-Team Start
+	function loadMarkers() {
+		$.getJSON('./js/locations.json', function(data) {
+			markers = data.map(function(location, i) {
+				return new google.maps.Marker({
+					position: location,
+					icon: "./images/pinkball.png", 
+					animation: google.maps.Animation.BOUNCE,
+				});
+			});
+			console.info(markers);
+			
+			// cluster markers X-Team Start
+			var markerCluster = new MarkerClusterer(map, markers, {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+			// cluster markers X-Team end
+		});
+	}
+	loadMarkers();
+	// load markers & cluster them X-Team end
+	
+	
+	
+	// test marker X-Team Start
 	var testMarker = new google.maps.Marker({
 		position: centerPointX,
 		icon: "./images/pinkball.png", 
 		animation: google.maps.Animation.BOUNCE,
 		map: map
 	});
+	// test marker X-Team end
 	
-	var markers = locations.map(function(location, i) {
-		return new google.maps.Marker({
-			position: location,
-			icon: "./images/pinkball.png", 
-			animation: google.maps.Animation.BOUNCE,
-		});
-	});
-	
+	// info window X-Team start
 	var infowindow = new google.maps.InfoWindow({
-		content: "In the name of Allah"
+		content: '<img src="./images/ratingX.png">' + '<a href="#">Ford Mustang 2017</a>'
 	});
 	testMarker.addListener('click', function () {
 		infowindow.open(map, testMarker);
 	});
+	// info window X-Team end
 	
-	var markerCluster = new MarkerClusterer(map, markers, {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
 }
